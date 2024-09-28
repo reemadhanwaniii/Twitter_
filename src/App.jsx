6b50b65c1386c6d104433248f4bf8776e77473d9
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-09-28 18:33:12 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Tweet from "./Components/Tweet/Tweet"
+
+function App() {
+ 
+
+  return (
+    <>
+      <Tweet content={"first tweet"} likeCount={78}/>
+    </>
+  )
+}
+
+export default App
