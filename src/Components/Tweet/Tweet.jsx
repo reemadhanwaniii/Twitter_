9222c97fb47dc59d7f6b6bfacdaee40dbd10453c
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-09-28 19:08:00 +0530

    added css

--- src/Components/Tweet/Tweet.jsx
+++ src/Components/Tweet/Tweet.jsx
@@ -1,10 +1,13 @@
+import "./Tweet.css"
+
+
 function Tweet({content , likeCount}) {
     //console.log("inside tweet")
     return(
-        <>
+        <div className="tweet-wrapper">
             <div className="tweet-content">{content}</div>
             <div className="tweet-like">{likeCount} likes</div>
-        </>
+        </div>
     )
 }
 
--- src/Components/TweetList/TweetList.jsx
+++ src/Components/TweetList/TweetList.jsx
@@ -1,12 +1,13 @@
-import Tweet from "../Tweet/Tweet"
+import Tweet from "../Tweet/Tweet";
+import './TweetList.css'
 
 function TweetList({tweets}) {
     //console.log("Reach here")
     return(
-        <ul>
+        <ul className="tweet-list">
             {
                 tweets.map((tweet)=>(
-                    <li key={tweet.id}>
+                    <li key={tweet.id} className="tweet-like-wrapper">
                         <Tweet content={tweet.content} likeCount={tweet.likeCount}/>
                     </li>
                 ))
