7af971ba46a3a42310efcfe922afd6bc75e52e60
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-10-03 19:37:36 +0530

    sort tweet acc to time

--- src/Components/Twitter/Twitter.jsx
+++ src/Components/Twitter/Twitter.jsx
@@ -39,10 +39,17 @@ function Twitter(){
             )
       }
 
+      const sortTweets =() => {
+        tweets.sort((t1,t2) => t2.createdAt.getTime() - t1.createdAt.getTime());
+        setTweets([...tweets]);
+      }
+
       return (
         <>
          <AddTweet onAddTweet={handleAddTweet} />
-         <button>Sort Tweet by Created At</button>
+         <button onClick={sortTweets}>
+            Sort Tweet by Created At
+        </button>
          <TweetList tweets={tweets} onEditTweet = {handleEditTweet}/>
         </>
       )
